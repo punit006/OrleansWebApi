a703dc12cd760acb0f7f28f1fb083d73c138cef0
Unity <teamunity.developer@gmail.com>
2022-04-25 17:11:16 +0530

    Grain Activation Issue Resolved

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IClusterClient>(orleansClient);
+builder.Services.AddSingleton<IGrainFactory>(orleansClient);
 
 
 var app = builder.Build();
